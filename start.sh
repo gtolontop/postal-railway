19d4e814723bc30ee807c3837c80166b67308664
Gtol <162237671+gtolontop@users.noreply.github.com>
2025-11-29 13:14:53 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@
   cat /config/postal.yml
   postal initialize || true
   echo "=== Creating admin user ==="
-  echo -e "admin@fivelink.lol\nFivelink\nAdmin\motdepasse" | postal make-user || echo "User exists"      
-  echo "=== Starting server ==="
-  exec /docker-entrypoint.sh "$@"
+  echo -e "admin@fivelink.lol\nFivelink\nAdmin\nmotdepasse" | postal make-user || echo "User exists"
+  echo "=== Starting worker in background ==="
+  postal run-worker &
+  echo "=== Starting web server ==="
+  postal run-web
