a0a1b8dcd5d1462b6d2c6564135ba0b7f9c0e5e5
Gtol <162237671+gtolontop@users.noreply.github.com>
2025-11-29 12:04:26 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,12 @@
   envsubst < /postal-template.yml > /config/postal.yml
   cat /config/postal.yml
   postal initialize || true
+  echo "=== Creating admin user ==="
+  postal make-user <<EOF
+  admin@fivelink.lol
+  Fivelink
+  Admin
+  motdepasse
+  EOF
+  echo "=== Starting server ==="
   exec /docker-entrypoint.sh "$@"
